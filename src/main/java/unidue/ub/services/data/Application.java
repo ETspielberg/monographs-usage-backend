456616016924be22f5e45fd8bf7300222449c400
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-01-17 06:31:45 +0100

    adjusting spring cloud release and cleaning up

--- src/main/java/unidue/ub/services/data/Application.java
+++ src/main/java/unidue/ub/services/data/Application.java
@@ -3,12 +3,17 @@ package unidue.ub.services.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
 @SpringBootApplication
 @EntityScan("unidue.ub.media.analysis")
+@EnableEurekaClient
 public class Application extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
@@ -17,10 +22,17 @@ public class Application extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().disable().csrf()
-				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		http.httpBasic().disable()
+				.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		http.authorizeRequests()
-				.anyRequest().hasIpAddress("127.0.0.1").anyRequest().permitAll().and()
-				.authorizeRequests().anyRequest().authenticated().anyRequest().permitAll();
+				.anyRequest().hasIpAddress("127.0.0.1")
+				.anyRequest().authenticated();
 	}
+
+	/*
+	@Bean
+	HeaderHttpSessionStrategy sessionStrategy() {
+		return new HeaderHttpSessionStrategy();
+	}
+	*/
 }
--- src/main/java/unidue/ub/services/data/ApplicationConfiguration.java
+++ src/main/java/unidue/ub/services/data/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package unidue.ub.services.data;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import unidue.ub.media.analysis.*;
 
 @Configuration
@@ -10,9 +11,6 @@ public class ApplicationConfiguration extends RepositoryRestConfigurerAdapter {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        config.getCorsRegistry()
-                .addMapping("/**")
-                .allowedOrigins("http://localhost:4200").allowedMethods("GET", "PUT", "DELETE", "POST");
         config.exposeIdsFor(Counter.class, Documentanalysis.class, Eventanalysis.class, JournalRequest.class, Nrequests.class, Shelfanalysis.class);
     }
 }
