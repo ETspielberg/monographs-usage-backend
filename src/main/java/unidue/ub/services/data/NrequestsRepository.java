9b2c230e5f86ad3a3a777bcdd25e21e1d991cece
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-03-06 16:13:23 +0100

    changes in Nrequestsrepository

--- src/main/java/unidue/ub/services/data/NrequestsRepository.java
+++ src/main/java/unidue/ub/services/data/NrequestsRepository.java
@@ -19,7 +19,10 @@ public interface NrequestsRepository  extends PagingAndSortingRepository<Nreques
     @Query(value = "SELECT p.* FROM Nrequests p WHERE SUBSTRING(p.shelfmark,1,3) BETWEEN :startNotation AND :endNotation", nativeQuery = true)
     public List<Nrequests> getNrequestsForNotationgroup(@Param("startNotation") String startNotation, @Param("endNotation") String endNotation);
 
-    @Query(value = "SELECT p.* FROM Nrequests p WHERE SUBSTRING(p.shelfmark,1,3) BETWEEN :startNotation AND :endNotation AND ratio >= :ratio AND nrequests >= :thresholdNrequests AND total_duration >= :duration AND date >= :startDate" +
+    @Query(value = "SELECT p.* FROM Nrequests p WHERE SUBSTRING(p.shelfmark,1,3) BETWEEN :startNotation AND :endNotation AND ratio >= :ratio AND nrequests >= :thresholdNrequests AND total_duration >= :duration SELECT mab, shelfmark, MAX(ratio) AS ratio,\n" +
+            "       MAX(nitems) AS nitmes, MAX(nlendable) AS nlendable, MAX(nloans) AS nloans, MAX(nrequests) AS nrequests, title_id, MAX(total_duration) as total_duration\n" +
+            "  FROM nrequests\n" +
+            "  WHERE SUBSTRING(shelfmark,1,3) BETWEEN :startNotation AND :endNotation AND ratio >= :ratio AND nrequests >= :thresholdNrequests AND total_duration >= :duration AND date >= :startDate GROUP BY mab,shelfmark,title_id" +
             "", nativeQuery = true)
     public List<Nrequests> getFilteredNrequestsForNotationgroup(@Param("startNotation") String startNotation, @Param("endNotation") String endNotation, @Param("ratio") Double ratio, @Param("duration") Integer duration, @Param(value="thresholdNrequests") Integer thresholdNrequests, @Param("startDate") @Temporal(TemporalType.TIMESTAMP) Date startDate);
 }
